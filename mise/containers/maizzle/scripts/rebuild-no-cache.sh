71db84d9ea104ce46ad7ad95db4c523c0329559f
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-03-06 21:52:35 -0800

    Change volume mapping from roo to ./src to /artifatct/src

---
+++ mise/containers/maizzle/scripts/rebuild-no-cache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker compose down
+# docker image rm $(docker images handsomefencer/maizzle*)
+# docker volume rm $(docker volume ls --filter name=maizzle) 
+# docker system prune -f
+docker images
+docker volume ls
+sudo rm -rf app builds
+dc run --rm maizzle
+
