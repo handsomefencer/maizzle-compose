d0a3b6ce555c8ef679ce1040efde49bc03133c11
Fred Schoeneman <fred.schoeneman@gmail.com>
2024-03-06 22:25:18 -0800

    Solid

--- mise/containers/maizzle/scripts/rebuild-no-cache.sh
+++ mise/containers/maizzle/scripts/rebuild-no-cache.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker compose down
-# docker image rm $(docker images handsomefencer/maizzle*)
-# docker volume rm $(docker volume ls --filter name=maizzle) 
-# docker system prune -f
+docker image rm $(docker images handsomefencer/maizzle*)
+docker volume rm $(docker volume ls --filter name=maizzle) 
+docker system prune -f
 docker images
 docker volume ls
 sudo rm -rf app builds
